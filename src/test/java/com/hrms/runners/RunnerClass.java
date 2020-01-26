4fd01301ccf91914582cf7e8b04dd95ce0930a4b
ntxm <51963319+ntxm@users.noreply.github.com>
2020-01-26 12:06:06 -0500

    Feature file created

---
+++ src/test/java/com/hrms/runners/RunnerClass.java
@@ -0,0 +1,15 @@
+package com.hrms.runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="src/test/resources/features/Login.feature"
+		)
+
+public class RunnerClass {
+
+}
---
+++ src/test/resources/features/Login.feature
@@ -0,0 +1,8 @@
+Feature: Login
+
+Scenario: Valid login
+
+Given I open browser and navigated to the HRMS
+When I enter valid username and valid password 
+And I click on the login button
+Then I successfully logged in
